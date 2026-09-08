f07605759e621bacc51d511e7e5ec60a4e3597d8
abdulhakim84 <54586446+abdulhakim84@users.noreply.github.com>
2026-09-08 15:33:06 +0700

    Refactor BleReceiver class by removing unused code
    Removed unused includes and methods from BleReceiver class.

--- main/ble_receiver.h
+++ main/ble_receiver.h
@@ -1,29 +1,9 @@
 #ifndef BLE_RECEIVER_H
 #define BLE_RECEIVER_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "nimble/nimble_port.h"
-#include "nimble/nimble_port_freertos.h"
-#include "host/ble_hs.h"
-#include "services/gap/ble_svc_gap.h"
-#include "services/gatt/ble_svc_gatt.h"
-
 class BleReceiver {
 public:
-    BleReceiver();
-    void Init();
-    static void StartAdvertising();
-    
-    // Dipindahkan ke public agar bisa diakses oleh tabel struct gatt_svcs
-    static int GattAccessCallback(uint16_t conn_handle, uint16_t attr_handle,
-                                   struct ble_gatt_access_ctxt *ctxt, void *arg);
-
-private:
-    static int GapEventHandler(struct ble_gap_event *event, void *arg);
-    static void HostTask(void *param);
-    static void OnReset(int reason);
-    static void OnSync();
+    static void Init();
 };
 
 #endif // BLE_RECEIVER_H
